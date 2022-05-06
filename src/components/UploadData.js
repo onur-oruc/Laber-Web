091e3e2a78c876acdf2bd98a25d59e1c8a647a9c
onur-oruc <onurorucbilkent@gmail.com>
2022-05-06 04:18:27 +0300

    make UploadData component return an empty component as it will not be used anymore

--- src/components/UploadData.js
+++ src/components/UploadData.js
@@ -5,27 +5,28 @@ import Button from '@material-ui/core/Button';
 const UploadData = () => {
  
   return (
-    <div style={{
-      display: 'flex',
-      margin: 'auto',
-      width: 400,
-      flexWrap: 'wrap',
-    }}>
-      <div style={{ width: '100%', float: 'left' }}>
-        <h3>Upload Your Data (Optional)</h3> 
-      </div>
-      <input
-        type="file"
-        accept="image/*"
-        style={{ display: 'none' }}
-        id="contained-button-file"
-      />
-      <label htmlFor="contained-button-file">
-        <Button style={{ marginLeft: 155 }} variant="contained" color="primary" component="span">
-          Upload
-        </Button>
-      </label>
-    </div>
+    <></>
+    // <div style={{
+    //   display: 'flex',
+    //   margin: 'auto',
+    //   width: 400,
+    //   flexWrap: 'wrap',
+    // }}>
+    //   {/* <div style={{ width: '100%', float: 'left' }}>
+    //     <h3>Upload Your Data (Optional)</h3> 
+    //   </div>
+    //   <input
+    //     type="file"
+    //     accept="image/*"
+    //     style={{ display: 'none' }}
+    //     id="contained-button-file"
+    //   />
+    //   <label htmlFor="contained-button-file">
+    //     <Button style={{ marginLeft: 155 }} variant="contained" color="primary" component="span">
+    //       Upload
+    //     </Button>
+    //   </label> */}
+    // </div>
   );
 }
 
