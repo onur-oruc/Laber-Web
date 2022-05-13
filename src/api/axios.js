2eab53798854fa900f7b56da08713034cde37f63
mericadil <adilmeric13@gmail.com>
2022-05-13 13:40:06 +0300

    last changes

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create( {
     // baseURL: 'http://laber-env.eba-65gdmegc.us-east-1.elasticbeanstalk.com'
-    baseURL: 'http://192.168.1.171:5000'
+    baseURL: 'http://localhost:5000'
 });
